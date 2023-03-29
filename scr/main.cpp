50187e704cbddf161b12cb84d1549df2a5e86517
Samuel-Brum <samuelbrummartins@gmail.com>
2023-03-29 03:49:22 -0300

    ultima do dia juro

--- scr/main.cpp
+++ scr/main.cpp
@@ -6,6 +6,7 @@
 #define FAT 20
 
 int main (int argc, char **argv) {
+  // TODO: implementar argc e argv para controlar chamada via makefile
   for (int i = 1; i <= 20; i++){
     std::cout << "Fib Recursivo: " << fibonacciRecursivo(i) << '\n'  
               << "Fib Iterativo: " << fibonacciIterativo(i) << '\n';
